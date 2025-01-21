0d42d29bd7893505547edff1b800bd7d4ee2cde3
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2025-01-21 16:23:31 +0500

    learning: main.go go.mod packages modules build

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/meharsulaiman/pggs
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Hello, world", rand.Intn(10), math.Pi)
+}
