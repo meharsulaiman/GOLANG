00e149fe7610b325bd2c93155f898c60775fee00
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2025-01-30 20:47:45 +0500

    another way to return

--- main.go
+++ main.go
@@ -12,9 +12,16 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	// fmt.Println("Hello, world", add(rand.Intn(10), 20), math.Pi)
 	a, b := swap("Hello", "World")
-
+	x, y := split(10)
+	fmt.Println(x, y)
 	fmt.Println(a, b)
 }
